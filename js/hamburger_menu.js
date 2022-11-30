6c0dfd55501da55f8e2575613847fb5ae72b0f2f
dabergotzDesign <otakulp@gmx.de>
2022-11-30 13:46:37 +0100

    media query, last adjustments

--- js/hamburger_menu.js
+++ js/hamburger_menu.js
@@ -1,6 +1,15 @@
 const button = document.getElementById("hamburgerButton");
 const nav = document.getElementById("navigationTop");
 
+const socialsBtn = document.getElementById("socialBtn");
+const socials = document.getElementById("socials");
+const arrow = document.querySelector(".fa-chevron-up");
+
 button.addEventListener('click', () => {
   nav.classList.toggle('show');
+});
+
+socialsBtn.addEventListener('click', ()=>{
+  socials.classList.toggle('showSocials');
+  arrow.classList.toggle('fa-rotate-180');
 });
